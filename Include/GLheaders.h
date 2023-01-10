7591eb2eed8fc3879ebea6c6ec6c43ababce37f0
Gary Deschaines <gedeschaines@gmail.com>
2023-01-09 18:03:34 -0600

    Added Linux porting

--- Include/GLheaders.h
+++ Include/GLheaders.h
@@ -1,9 +1,10 @@
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>		// The windows header file needs to be here...
-#include <gl/gl.h>			// Header File For The OpenGL32 Library
-#include <gl/glu.h>			// Header File For The GLu32 Library
-
-
-
+#ifdef __linux__
+  #include <GL/glu.h>
+  #include <GL/gl.h>
+#else
+  #define WIN32_LEAN_AND_MEAN
+  #include <windows.h>		// The windows header file needs to be here...
+  #include <gl/gl.h>		// Header file for The OpenGL32 library
+  #include <gl/glu.h>		// Header file for the GLu32 library
+#endif
 
